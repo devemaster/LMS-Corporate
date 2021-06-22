dbfa609b4439e1016e7d661edcd8729c3c5677f4
Kapil Karda <kapil.karda@gmail.com>
2021-06-22 13:22:09 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './Pages/index/index.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
-  {path:'main', component:IndexComponent},
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./Pages/home/home.module').then(m => m.HomeModule) },
-  { path: 'index', loadChildren: () => import('./Pages/index/index.module').then(m => m.IndexModule) },
+  { path: '', loadChildren: () => import('./Pages/index/index.module').then(m => m.IndexModule) },
   { path: 'rtl', loadChildren: () => import('./Pages/rtl/rtl.module').then(m => m.RtlModule) },
   { path: 'ltr', loadChildren: () => import('./Pages/ltr/ltr.module').then(m => m.LtrModule) },
   { path: '**', component: IndexComponent}
